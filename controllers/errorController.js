9231705e55df4aff2eda672d861621edeaf4e33f
aibazhang <dtjp@tutanota.com>
2022-02-23 09:31:44 +0000

    add: handling mongoDB error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,20 @@
+const { del } = require('express/lib/application');
 const AppError = require('../utils/appError');
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.message.match(/"(.*?)"/);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
@@ -41,8 +54,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
+    let error = Object.assign(err);
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    // if (error.name === 'ValidatorError') error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
