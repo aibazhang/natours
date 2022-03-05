cb5a361f983ec818ba6b66c15e7119c30e668274
aibazhang <dtjp@tutanota.com>
2022-03-05 06:30:06 +0000

    add: sending JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,19 @@ const siginToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = siginToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
